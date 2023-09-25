8dbf05e7130297a2276b575f2890a0b94ba310b6
Mai Elkhodery <105084125+MaiElkhodery@users.noreply.github.com>
2023-09-25 23:39:29 +0300

    Revert "Origin/features/login UI"

--- app/src/main/java/com/example/retrofit/presentation/signupscreen/AuthText.kt
+++ app/src/main/java/com/example/retrofit/presentation/signupscreen/AuthText.kt
@@ -2,6 +2,7 @@ package com.example.retrofit.presentation.signupscreen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
